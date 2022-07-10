2592afb011e1a5550f6f7ff0362883305572959f
birhanus <birhanushimles716@gmail.com>
2022-07-10 20:00:18 +0000

    fix pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -11,6 +11,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         parameters.
 
     """
-    start = (page -1 ) * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
